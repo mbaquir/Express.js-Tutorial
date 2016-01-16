b365fe18b76d40c387e25c0d3ac7c798e40ade20
mbaquir <mohammed.baquir52@gmail.com>
2016-01-15 22:56:33 -0500

    Json

---
+++ jsonme.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var fs = require('fs');
+
+var app = express();
+
+app.get('/books',function(req,res,error){
+
+    fs.readFile(process.argv[3], function(data){
+
+    	object = JSON.parse(data)
+
+    	res.json(object);
+
+    })
+
+
+})
+
+app.listen(process.argv[2])
